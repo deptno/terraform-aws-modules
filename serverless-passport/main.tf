49f447bdbb3cf23499e8194e78f852ea1e256d3a
bglee <deptno@gmail.com>
2019-09-10 22:22:23 +0900

    fix: dynamodb billing mode

--- serverless-passport/main.tf
+++ serverless-passport/main.tf
@@ -2,6 +2,7 @@ resource aws_dynamodb_table token {
   name = var.token-table-name
   hash_key = var.token-table-hash-key
   range_key = var.token-table-range-key
+  billing_mode = "PAY_PER_REQUEST"
 
   ttl {
     attribute_name = var.token-table-ttl-key
