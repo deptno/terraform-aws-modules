320b5cd0eab590c22b0b0eca3f16fd1e1c4ed607
bglee <deptno@gmail.com>
2019-12-15 12:21:45 +0900

    fix: yiguana, more output

--- yiguana/output.tf
+++ yiguana/output.tf
@@ -1,9 +1,12 @@
 output dynamodb {
 	value = aws_dynamodb_table.yiguana.name
 }
-output s3 {
+output s3_bucket {
 	value = aws_s3_bucket.yiguana.bucket
 }
-output cloudfront_frontend {
+output cdn_domain {
+	value = aws_route53_record.cdn.name
+}
+output cloudfront_frontend_id {
 	value = aws_cloudfront_distribution.yiguana.id
 }
