3201b9094d2523fd39b084d65283b98858bd572d
bglee <deptno@gmail.com>
2019-12-27 16:42:12 +0900

    fix: module path

--- layers/main.tf
+++ layers/main.tf
@@ -1,6 +1,6 @@
 resource aws_lambda_layer_version chrome {
   layer_name = "chrome"
-  filename = "chrome_aws_lambda.zip"
+  filename = "${path.module}/chrome_aws_lambda.zip"
   compatible_runtimes = [
     "nodejs12.x"
   ]
@@ -9,7 +9,7 @@ resource aws_lambda_layer_version chrome {
 resource aws_lambda_layer_version font_noto_cjk_regular {
   description = "Noto CJK Sans, Serif regular"
   layer_name = "font_noto_cjk_regular"
-  filename = "font_noto_cjk_regular.zip"
+  filename = "${path.module}/font_noto_cjk_regular.zip"
   compatible_runtimes = [
     "nodejs12.x"
   ]
@@ -18,7 +18,7 @@ resource aws_lambda_layer_version font_noto_cjk_regular {
 resource aws_lambda_layer_version font_color_emoji {
   layer_name = "font_color_emoji"
   description = "Color emoji"
-  filename = "font_color_emoji.zip"
+  filename = "${path.module}/font_color_emoji.zip"
   compatible_runtimes = [
     "nodejs12.x"
   ]
